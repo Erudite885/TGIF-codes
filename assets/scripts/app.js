5cece146fe6ea0a79e73734a3f72c2c94b1a0bf1
Erudite885 <erudite885@gmail.com>
2023-01-28 10:03:17 +0100

    triggering requests via the UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 const listEl = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -44,5 +46,10 @@ async function createPost(title, content) {
   sendHttpRequest("POST", "http://jsonplaceholder.typicode.com/posts", post);
 }
 
-fetchPosts();
-createPost("dummdum", "a dumdum post");
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", event =>{
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
+  const enteredContent = event.currentTarget.querySelector('#content').value;
+  createPost(enteredTitle, enteredContent);
+})
