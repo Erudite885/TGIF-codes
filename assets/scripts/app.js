73ab0993072958401e2148d2d0f5a3388b2fe390
Erudite885 <erudite885@gmail.com>
2023-01-28 09:38:52 +0100

    promise to http requests

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,36 @@
 const listEl = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://jsonplaceholder.typicode.com/posts");
+    xhr.open(method, url);
 
-xhr.responseType = "json";
+    xhr.responseType = "json";
 
-xhr.onload = function () {
-  //   const listOfPosts = JSON.parse(xhr.response)
-  const listOfPosts = xhr.response;
-  console.log(listOfPosts);
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //   const listOfPosts = JSON.parse(xhr.response)
+    };
+
+    xhr.send();
+  });
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "http://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responseData;
+  // console.log(listOfPosts);
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
     listEl.append(postEl);
   }
-};
-
-xhr.send();
+}
+fetchPosts();
